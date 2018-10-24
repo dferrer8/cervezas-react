163819c000a255f9cdeb64b56a1094cb5dfc7292
dferrer8 <david.dfg@gmail.com>
2018-10-24 16:25:51 +0200

    Componentes creados base

---
+++ src/CervezasList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CervezasList extends Component {
+  render() {
+    return (
+      <div>
+        <p>Aquí estaría la lista de cervezas</p>
+      </div>
+    )
+  }
+}
--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
+import SearchBox from './SearchBox'
+import CervezasList from './CervezasList'
 import './Main.css'
 
 export class Main extends Component {
   render() {
     return (
       <main>
-        <Searchbox />
+        <SearchBox />
         <CervezasList />
       </main>
     )
