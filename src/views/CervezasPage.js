6935ab1e5fb4d6b4f25a0a0e699ae72400a8fff3
dferrer8 <david.dfg@gmail.com>
2018-10-25 17:36:24 +0200

    añadido con mensaje para errores generados

--- src/views/CervezasPage.js
+++ src/views/CervezasPage.js
@@ -11,22 +11,28 @@ export default class CervezasPage extends Component {
   }
 
   state = {
-    cervezas: []
+    cervezas: [],
+    error: ''
   }
   componentDidMount =  () => {
+    this.setState({error: ''}) // inicializo el error
     fetch('http://localhost:8080/api/cervezas')
       .then(response => response.json())
       .then(cervezas => this.setState({ cervezas }))
-      .catch(error =>{})
+      .catch(error =>{
+        //console.log(`error: ${error}`)
+        this.setState({ error: error.message }) // por si recibo otro error
+      })
   }
 
 
   render() {
-    const { cervezas } = this.state
+    const { cervezas, error } = this.state
     return (
       <div>
       <SearchBox />
       <CervezasList cervezas = {cervezas}/>
+      {error ? <p>{error}</p> :''}
     </div>
     )
   }
