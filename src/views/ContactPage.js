254191252603845f7718e94f439ed34c02e017a1
dferrer8 <david.dfg@gmail.com>
2018-10-25 19:58:35 +0200

    se ha roto

--- src/views/ContactPage.js
+++ src/views/ContactPage.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { GoogleMap, Marker,w ithScriptjs, withGoogleMap  } from "react-google-maps"
+
+const MyMapComponent = (props) =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+  </GoogleMap>
+
+{/* <MyMapComponent isMarkerShown />// Map with a Marker
+<MyMapComponent isMarkerShown={false} />// Just only Map */}
 
 export default function ContactPage() {
   return (
     <div>
       <p>Pagina contacto</p>
+      <MyMapComponent isMarkerShown />/
     </div>
   )
 }
